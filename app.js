b15ee884365e021ae854f935bb0b0aff657943bd
m7md0a <m7md0a.work@gmail.com>
2024-04-02 20:41:45 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+import Tree from "./bts.js"
+
+let tree = new Tree()
+
+tree.insert("ahmed")
+tree.insert("hamo")
+tree.insert("hady")
+tree.insert("ali")
+tree.insert("mohamed")
+
+
+let r = tree.search('moha')
+console.log(tree.size());
+// console.log(r.right);
+// console.log(r.right.right);
+// console.log(r.right.right.right);
---
+++ bts.js
@@ -0,0 +1,81 @@
+
+class Node {
+    constructor (value) {
+        this.value = value;
+        this.right = null;
+        this.left = null;
+    }
+}
+
+class Tree {
+    constructor() {
+        this.root = null
+        this.count = 0
+    }
+
+    size() {
+        return this.count
+    }
+
+    insert(value) {
+        let newNode = new Node(value)
+        this.count++
+        if (!this.root) {
+            this.root = newNode
+        } else {
+            let currentNode = this.root;
+            while (true) {
+                if (newNode.value < currentNode.value) {
+                    if (!currentNode.left) {
+                        currentNode.left = newNode
+                        return this
+                    }
+                    currentNode = currentNode.left
+                } else {
+                    if (!currentNode.right) {
+                        currentNode.right = newNode
+                        return this
+                    }
+                    currentNode = currentNode.right
+                }
+            }
+        }
+    }
+
+    min() {
+        let currentNode = this.root;
+        while (currentNode.left) {
+            currentNode = currentNode.left
+        }
+        return currentNode.value
+    }
+
+    max() {
+        let currentNode = this.root;
+        while (currentNode.right) {
+            currentNode = currentNode.right
+        }
+        return currentNode.value
+    }
+
+    search(value) {
+        if (!this.root) return false
+
+        let current = this.root
+        let found = false
+        while (current && !found) {
+            if (value < current.value) {
+                current = current.left
+            } else if (value > current.value){
+                current = current.right
+            } else {
+                found = current
+            }
+        }
+
+        if (!found) return undefined
+        return found
+    }
+}
+
+export default Tree
